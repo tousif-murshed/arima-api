6790890e4f1a646f3e12cf58bfa4a4d68b11f943
Tousif Murshed <tousif.murshed@wal-mart.com>
2019-11-13 15:48:55 -0600

    correct error type

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from utilities import json_to_csv
 from utilities import csv_to_json
 import os
+from os import environ
 
 app = Flask(__name__)
 csv_path = "./tmp/data.csv"
@@ -9,12 +10,12 @@ csv_path = "./tmp/data.csv"
 
 @app.before_first_request
 def create_temp():
+    print("The environment variable value is {}".format(environ["TEST"]))
     try:
         cwd = os.getcwd()
         os.mkdir("{}/tmp".format(cwd))
-        print(os.environ["TEST"])
-    except IOError:
-        print("Unable to create tmp directory")
+    except OSError as err:
+        print("Unable to create tmp directory -> {}".format(err))
 
 
 @app.route("/")
