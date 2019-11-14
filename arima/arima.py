58a8991c74ad6c9760f36a00bf010a09a767f5cb
Tousif Murshed <tousif.murshed@wal-mart.com>
2019-11-14 00:55:55 -0600

    cleanup

--- arima/arima.py
+++ arima/arima.py
@@ -2,26 +2,6 @@ import pandas as pd
 from pmdarima import auto_arima
 
 
-# data = pd.read_csv('dataPhSales.csv', index_col=0)
-# data.index = pd.to_datetime(data.index)
-# print(data.head())
-#
-# stepwise_model = auto_arima(data, start_p=0, start_q=0,
-#                             max_p=3, max_q=3, m=7,
-#                             start_P=0, seasonal=True,
-#                             d=0, D=0, trace=True,
-#                             error_action='ignore',
-#                             suppress_warnings=True,
-#                             stepwise=True)
-# print(stepwise_model.aic())
-#
-# train = data.loc['2019-01-01':'2019-11-07']
-# test = data.loc['2019-11-08':]
-# stepwise_model.fit(train)
-# future_forecast = stepwise_model.predict(n_periods=31)
-# print(future_forecast)
-
-
 class Arima:
 
     def __init__(self, data_set_path):
@@ -40,7 +20,8 @@ class Arima:
             self.model.fit(training_model)
             number_of_days = int(forecast_number_of_weeks) * 7
             forecast_data = [int(fct) for fct in self.model.predict(number_of_days)]
-            forecast_date_range = [date.strftime('%Y-%m-%d') for date in pd.date_range(start=forecast_start_date, periods=number_of_days)]
+            forecast_date_range = [date.strftime('%Y-%m-%d') for date in
+                                   pd.date_range(start=forecast_start_date, periods=number_of_days)]
             return zip(forecast_date_range, forecast_data)
         except Exception as e:
             print("Error while forecasting -> {}".format(e))
