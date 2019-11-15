95a2ab195676700a7d12d1d0107fb0fcd2b85948
Tousif Murshed <tousif.murshed@wal-mart.com>
2019-11-15 16:12:09 -0600

    testing

--- arima/train_arima.py
+++ arima/train_arima.py
@@ -6,7 +6,7 @@ import pandas as pd
 def fetch_training_set(training_data_path):
     try:
         print("connection string", environ.get("MONGO_CONNECTION"))
-        client = pymongo.MongoClient(environ.get("MONGO_CONNECTION"))
+        client = pymongo.MongoClient("mongodb+srv://default:gIt0f8tDqjnvnJfr@cluster0-um8zf.azure.mongodb.net/history?retryWrites=true&w=majority")
         db = client.history
         training_set = db.training_set
         data = list(data for data in training_set.find())
